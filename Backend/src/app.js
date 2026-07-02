a4a0cd524b7b3d4d79075135cdb972aa7fe43aaf
Nemat jamal <dilkashjamal8@gmail.com>
2026-07-03 00:18:40 +0530

    first commit

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -14,16 +14,18 @@ import codingRoutes from "./routes/coding.route.js";
 dotenv.config();
 
 const app = express();
-
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);  Q1``
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
-  })
-);
+
 
 app.get("/", (req, res) => {
   res.send("API Running...");
